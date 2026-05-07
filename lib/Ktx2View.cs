980a3db924e94b9327ac0b4ce696aac785e6a659
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2026-05-07 14:27:15 +0300

    First version of Ktx2View
    Includes some test cases

---
+++ lib/Ktx2View.cs
@@ -0,0 +1,285 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Buffers.Binary;
+
+namespace KtxSharp;
+
+/// <summary>
+/// Ktx2View class
+/// </summary>
+/// <remark>
+/// Does not store the data, but acts as view over existing data.
+/// Based on specifications mentioned in https://github.khronos.org/KTX-Specification/ktxspec.v2.html
+/// </remark>
+public sealed class Ktx2View
+{
+	private readonly Memory<byte> data;
+
+	/// <summary>
+	/// Only constructor
+	/// </summary>
+	/// <param name="input">Memory that contains Ktx2 data as bytes</param>
+	/// <param name="doSafetyChecks">Do safety checks during construction? (default: false)</param>
+	public Ktx2View(Memory<byte> input, bool doSafetyChecks = false)
+	{
+		this.data = input;
+
+		if (doSafetyChecks)
+		{
+			// TODO: Add these
+		}
+	}
+
+	/// <summary>
+	/// Get image format as Uint value
+	/// </summary>
+	/// <returns>Uint</returns>
+	public uint GetVkFormatUint()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(12).Span);
+	}
+
+	/// <summary>
+	/// Get image format as Vulkan VkFormat enum
+	/// </summary>
+	/// <returns>VkFormat</returns>
+	public VkFormat GetVkFormat()
+	{
+		uint value = this.GetVkFormatUint();
+		if (VkFormat.IsDefined(typeof(VkFormat), value))
+		{
+			return (VkFormat)value;
+		}
+
+		return VkFormat.ERROR;
+	}
+
+	/// <summary>
+	/// Get size of the data type in bytes used to upload the data to a graphics API
+	/// </summary>
+	/// <returns>Uint</returns>
+	public uint GetTypeSize()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(16).Span);
+	}
+
+	/// <summary>
+	/// The width of the texture image for level 0, in pixels.
+	/// </summary>
+	/// <returns>Pixel width</returns>
+	public uint GetPixelWidth()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(20).Span);
+	}
+
+	/// <summary>
+	/// The height of the texture image for level 0, in pixels.
+	/// </summary>
+	/// <returns>Pixel height</returns>
+	public uint GetPixelHeight()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(24).Span);
+	}
+
+	/// <summary>
+	/// The depth of the texture image for level 0
+	/// </summary>
+	/// <returns></returns>
+	public uint GetPixelDepth()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(28).Span);
+	}
+
+	/// <summary>
+	/// The number of array elements
+	/// </summary>
+	/// <returns></returns>
+	public uint GetLayerCount()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(32).Span);
+	}
+
+	/// <summary>
+	/// Specifies the number of cubemap faces
+	/// </summary>
+	/// <returns></returns>
+	public uint GetFaceCount()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(36).Span);
+	}
+
+	/// <summary>
+	/// The number of levels in the Mip Level Array
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>0 means that there is only one level and applications should generate other levels if needed. 1 means that there is only one level and no others are needed</remarks>
+	public uint GetLevelCount()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(40).Span);
+	}
+
+	/// <summary>
+	/// Supercompression scheme as Uint
+	/// </summary>
+	/// <returns>Uint</returns>
+	public uint GetSupercompressionSchemeUint()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(44).Span);
+	}
+
+	/// <summary>
+	/// Supercompression scheme
+	/// </summary>
+	/// <returns>SupercompressionScheme</returns>
+	public SupercompressionScheme GetSupercompressionScheme()
+	{
+		uint value = this.GetSupercompressionSchemeUint();
+
+		if (SupercompressionScheme.IsDefined(typeof(SupercompressionScheme), value))
+		{
+			return (SupercompressionScheme)value;
+		}
+
+		return SupercompressionScheme.ERROR;
+	}
+
+	/// <summary>
+	/// The offset from the start of the file to the dfdTotalSize field of the Data Format Descriptor
+	/// </summary>
+	/// <returns></returns>
+	public uint GetDfdByteOffset()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(48).Span);
+	}
+
+	/// <summary>
+	/// The total number of bytes in the Data Format Descriptor including the dfdTotalSize field. dfdByteLength must equal dfdTotalSize
+	/// </summary>
+	/// <returns></returns>
+	public uint GetDfdByteLength()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(52).Span);
+	}
+
+	/// <summary>
+	/// The offset from the start of the file to key/value data
+	/// </summary>
+	/// <returns></returns>
+	public uint GetKvdByteOffset()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(56).Span);
+	}
+
+	/// <summary>
+	/// The total number of bytes of key/value data including all keyAndValueByteLength fields, all keyAndValue fields and all valuePadding fields
+	/// </summary>
+	/// <returns></returns>
+	public uint GetKvdByteLength()
+	{
+		return BitConverter.ToUInt32(this.data.Slice(60).Span);
+	}
+
+	/// <summary>
+	/// The offset from the start of the file to supercompressionGlobalData
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>The value must be 0 when sgdByteLength = 0</remarks>
+	public ulong GetSgdByteOffset()
+	{
+		return BitConverter.ToUInt64(this.data.Slice(64).Span);
+	}
+
+	/// <summary>
+	/// The number of bytes of supercompressionGlobalData
+	/// </summary>
+	/// <returns></returns>
+	public ulong GetSgdByteLength()
+	{
+		return BitConverter.ToUInt64(this.data.Slice(72).Span);
+	}
+
+	/// <summary>
+	/// Indexes for each mip levels
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>Zero index [0] has the largest mip</remarks>
+	public List<LevelIndex> GetLevelIndexes()
+	{
+		uint mipLoops = Math.Max(1, this.GetLayerCount());
+		List<LevelIndex> returnValues = new List<LevelIndex>((int)mipLoops);
+
+		int startDataIndex = 80;
+		for (uint u = 0; u < mipLoops; u++)
+		{
+			ulong firstValue = BitConverter.ToUInt64(this.data.Slice(startDataIndex).Span);
+			ulong secondValue = BitConverter.ToUInt64(this.data.Slice(startDataIndex+8).Span);
+			ulong thirdValue = BitConverter.ToUInt64(this.data.Slice(startDataIndex+16).Span);
+			returnValues.Add(new LevelIndex(firstValue, secondValue, thirdValue));
+
+			startDataIndex += 24;
+		}
+
+		return returnValues;
+	}
+
+	/// <summary>
+	/// Indicates the total number of bytes in the dfDescriptor including dfdTotalSize
+	/// </summary>
+	/// <returns></returns>
+	public uint GetDfdTotalSize()
+	{
+		int startDataIndex = (int)this.GetDfdByteOffset();
+		return BitConverter.ToUInt32(this.data.Slice(startDataIndex).Span);
+	}
+
+	/// <summary>
+	/// Bytes of data format descriptor
+	/// </summary>
+	/// <returns></returns>
+	public Memory<byte> GetDataFormatDescriptorRaw()
+	{
+		int startDataIndex = (int)this.GetDfdByteOffset() + Common.sizeOfUint;
+		int totalSize = (int)this.GetDfdTotalSize() - Common.sizeOfUint;
+		return this.data.Slice(startDataIndex, totalSize);
+	}
+
+	/// <summary>
+	/// Metadata dictionary (key is string)
+	/// </summary>
+	/// <returns></returns>
+	public Dictionary<string, MetadataValue> GetMetadataDictionary()
+	{
+		int startDataIndex = (int)this.GetKvdByteOffset();
+		int totalSize = (int)this.GetKvdByteLength();
+		return Ktx2Metadata.ParseMetadata(this.data.Slice(startDataIndex, totalSize).Span);
+	}
+
+	/// <summary>
+	/// Get bytes of supercompression global data
+	/// </summary>
+	/// <returns></returns>
+	public Memory<byte> GetSupercompressionGlobalDataRaw()
+	{
+		int startDataIndex = (int)this.GetSgdByteOffset();
+		int totalSize = (int)this.GetSgdByteLength();
+		return this.data.Slice(startDataIndex, totalSize);
+	}
+
+	/// <summary>
+	/// Get all bytes of certain mip map level
+	/// </summary>
+	/// <param name="levelIndex">Zero based index</param>
+	/// <returns>all bytes of certain mip map level</returns>
+	/// <remarks>Zero index [0] has the smallest mip (order is reverse if compared to GetLevelIndexes())</remarks>
+	public Memory<byte> GetLevelImage(int levelIndex)
+	{
+		var orderedList = this.GetLevelIndexes();
+		orderedList.Sort((a,b) => a.byteOffset.CompareTo(b.byteOffset)); // Sort the copy
+
+		int startDataIndex = (int)orderedList[levelIndex].byteOffset;
+		int totalSize = (int)orderedList[levelIndex].byteLength;
+		return this.data.Slice(startDataIndex, totalSize);
+	}
+}
---
+++ tests/Ktx2ViewTests.cs
@@ -0,0 +1,124 @@
+using NUnit.Framework;
+using KtxSharp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tests;
+
+public class Ktx2ViewTests
+{
+	[Test]
+	public void CheckWithValidKtx2SamplesTest()
+	{
+		// Arrange
+		byte[] inputBytes1 = Ktx2Samples.ktx2Sample1;
+		byte[] inputBytes2 = File.ReadAllBytes(CommonFiles.validKtx2Sample1Filename);
+
+		// Act
+		Ktx2View view1 = new Ktx2View(inputBytes1);
+		List<LevelIndex> levelIndexes1 = view1.GetLevelIndexes();
+		Dictionary<string, MetadataValue> metadataDictionary1 = view1.GetMetadataDictionary();
+		Memory<byte> supercompressonGlobalDataRaw1 = view1.GetSupercompressionGlobalDataRaw();
+
+		Ktx2View view2 = new Ktx2View(inputBytes2);
+		List<LevelIndex> levelIndexes2 = view2.GetLevelIndexes();
+		Dictionary<string, MetadataValue> metadataDictionary2 = view2.GetMetadataDictionary();
+		Memory<byte> supercompressonGlobalDataRaw2 = view2.GetSupercompressionGlobalDataRaw();
+
+		// Assert
+		Assert.That(view1.GetVkFormatUint(), Is.EqualTo(0));
+		Assert.That(view1.GetVkFormat(), Is.EqualTo(VkFormat.VK_FORMAT_UNDEFINED));
+		Assert.That(view1.GetTypeSize(), Is.EqualTo(1));
+		Assert.That(view1.GetPixelWidth(), Is.EqualTo(8));
+		Assert.That(view1.GetPixelHeight(), Is.EqualTo(8));
+		Assert.That(view1.GetPixelDepth(), Is.EqualTo(0));
+		Assert.That(view1.GetSupercompressionSchemeUint(), Is.EqualTo(1));
+		Assert.That(view1.GetSupercompressionScheme(), Is.EqualTo(SupercompressionScheme.BasisLZ));
+
+		Assert.That(view1.GetDfdByteOffset(), Is.EqualTo(0x00000068));
+		Assert.That(view1.GetDfdByteLength(), Is.EqualTo(0x0000003C));
+
+		Assert.That(view1.GetKvdByteOffset(), Is.EqualTo(0x000000A4));
+		Assert.That(view1.GetKvdByteLength(), Is.EqualTo(0x00000058));
+
+		Assert.That(view1.GetSgdByteOffset(), Is.EqualTo(0x0000000000000100));
+		Assert.That(view1.GetSgdByteLength(), Is.EqualTo(0x000000000000008C));
+
+		Assert.That(levelIndexes1.Count, Is.EqualTo(1));
+		Assert.That(levelIndexes1[0].byteOffset, Is.EqualTo(0x000000000000018C));
+		Assert.That(levelIndexes1[0].byteLength, Is.EqualTo(0x0000000000000003));
+		Assert.That(levelIndexes1[0].uncompressedByteLength, Is.EqualTo(0x0000000000000000));
+
+		Assert.That(view1.GetDfdTotalSize(), Is.EqualTo(60));
+		Assert.That(view1.GetDataFormatDescriptorRaw().Length, Is.EqualTo(60 - Common.sizeOfUint));
+
+		Assert.That(metadataDictionary1.Count, Is.EqualTo(2));
+		Assert.That(metadataDictionary1.ContainsKey("KTXorientation"), Is.True);
+		Assert.That(metadataDictionary1["KTXorientation"].isString, Is.True);
+		Assert.That(metadataDictionary1["KTXorientation"].stringValue, Is.EqualTo("rd"));
+
+		Assert.That(metadataDictionary1.ContainsKey("KTXwriter"), Is.True);
+		Assert.That(metadataDictionary1["KTXwriter"].isString, Is.True);
+		Assert.That(metadataDictionary1["KTXwriter"].stringValue, Is.EqualTo("toktx v4.0.__default__ / libktx v4.0.__default__"));
+
+		Assert.That(view1.GetSgdByteLength(), Is.EqualTo(supercompressonGlobalDataRaw1.Length));
+
+		Assert.That(levelIndexes1[0].byteLength, Is.EqualTo(view1.GetLevelImage(0).Length));
+		Assert.That(view1.GetLevelImage(0).ToArray(), Is.EqualTo(new byte[] {0x4E, 0x0E, 0x04}));
+
+
+		Assert.That(view2.GetVkFormatUint, Is.EqualTo(23));
+		Assert.That(view2.GetVkFormat, Is.EqualTo(VkFormat.VK_FORMAT_R8G8B8_UNORM));
+		Assert.That(view2.GetTypeSize(), Is.EqualTo(1));
+		Assert.That(view2.GetPixelWidth(), Is.EqualTo(8));
+		Assert.That(view2.GetPixelHeight(), Is.EqualTo(8));
+		Assert.That(view2.GetPixelDepth(), Is.EqualTo(0));
+		Assert.That(view2.GetSupercompressionSchemeUint(), Is.EqualTo(0));
+		Assert.That(view2.GetSupercompressionScheme(), Is.EqualTo(SupercompressionScheme.None));
+
+		Assert.That(view2.GetDfdByteOffset(), Is.EqualTo(0x00000068));
+		Assert.That(view2.GetDfdByteLength(), Is.EqualTo(0x0000004C));
+
+		Assert.That(view2.GetKvdByteOffset(), Is.EqualTo(0x000000B4));
+		Assert.That(view2.GetKvdByteLength(), Is.EqualTo(0x00000030));
+
+		Assert.That(view2.GetSgdByteOffset(), Is.EqualTo(0x0000000000000000));
+		Assert.That(view2.GetSgdByteLength(), Is.EqualTo(0x0000000000000000));
+
+		Assert.That(levelIndexes2.Count, Is.EqualTo(1));
+		Assert.That(levelIndexes2[0].byteOffset, Is.EqualTo(0x00000000000000E4));
+		Assert.That(levelIndexes2[0].byteLength, Is.EqualTo(0x00000000000000C0));
+		Assert.That(levelIndexes2[0].uncompressedByteLength, Is.EqualTo(0x00000000000000C0));
+
+		Assert.That(view2.GetDfdTotalSize(), Is.EqualTo(76));
+		Assert.That(view2.GetDataFormatDescriptorRaw().Length, Is.EqualTo(76 - Common.sizeOfUint));
+
+		Assert.That(metadataDictionary2.Count, Is.EqualTo(1));
+		Assert.That(metadataDictionary2.ContainsKey("KTXorientation"), Is.False);
+		Assert.That(metadataDictionary2.ContainsKey("KTXwriter"), Is.True);
+		Assert.That(metadataDictionary2["KTXwriter"].isString, Is.True);
+		Assert.That(metadataDictionary2["KTXwriter"].stringValue, Is.EqualTo("ktx create v4.4.2 / libktx v4.4.2"));
+
+		Assert.That(view2.GetSgdByteLength(), Is.EqualTo(supercompressonGlobalDataRaw2.Length));
+
+		Assert.That(levelIndexes2[0].byteLength, Is.EqualTo(view2.GetLevelImage(0).Length));
+		Assert.That(view2.GetLevelImage(0).ToArray(), Is.EqualTo(new byte[] {0xFF, 0x00, 0x67, 0xE5, 0x01, 0x5D, 0xC2, 0x03, 0x51, 0xA3, 0x07, 0x46,
+																			0x86, 0x0D, 0x3C, 0x6F, 0x14, 0x33, 0x58, 0x1E, 0x2B, 0x46, 0x2A, 0x24,
+																			0xE2, 0x01, 0x5D, 0xC2, 0x03, 0x51, 0xA1, 0x07, 0x46, 0x85, 0x0D, 0x3C,
+																			0x6D, 0x14, 0x33, 0x57, 0x1E, 0x2B, 0x45, 0x2A, 0x23, 0x34, 0x39, 0x1D,
+																			0xC0, 0x03, 0x50, 0xA1, 0x07, 0x45, 0x86, 0x0D, 0x3B, 0x6D, 0x14, 0x33,
+																			0x58, 0x1E, 0x2A, 0x44, 0x2B, 0x23, 0x34, 0x3A, 0x1D, 0x26, 0x4A, 0x17,
+																			0xA1, 0x07, 0x45, 0x85, 0x0D, 0x3C, 0x6C, 0x15, 0x33, 0x56, 0x1F, 0x29,
+																			0x44, 0x2A, 0x23, 0x34, 0x3A, 0x1D, 0x27, 0x4A, 0x16, 0x1B, 0x60, 0x11,
+																			0x83, 0x0D, 0x3B, 0x6C, 0x15, 0x33, 0x56, 0x1F, 0x2A, 0x43, 0x2C, 0x23,
+																			0x33, 0x3A, 0x1D, 0x25, 0x4A, 0x17, 0x1A, 0x5F, 0x11, 0x11, 0x77, 0x0D,
+																			0x6C, 0x16, 0x32, 0x56, 0x20, 0x29, 0x43, 0x2C, 0x22, 0x33, 0x3B, 0x1C,
+																			0x26, 0x4C, 0x17, 0x1A, 0x60, 0x11, 0x11, 0x76, 0x0D, 0x0A, 0x8E, 0x0A,
+																			0x56, 0x1F, 0x29, 0x42, 0x2C, 0x23, 0x33, 0x3A, 0x1C, 0x25, 0x4C, 0x16,
+																			0x1A, 0x60, 0x11, 0x11, 0x77, 0x0D, 0x0A, 0x90, 0x09, 0x05, 0xAF, 0x06,
+																			0x42, 0x2C, 0x23, 0x33, 0x3B, 0x1C, 0x25, 0x4C, 0x17, 0x1A, 0x60, 0x11,
+																			0x11, 0x77, 0x0D, 0x0A, 0x90, 0x09, 0x05, 0xAF, 0x06, 0x02, 0xCE, 0x04}));
+	}
+}
